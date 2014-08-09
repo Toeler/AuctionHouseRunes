d2ad2ec7b89cc126335d4bc5099bc6bc86adaec7
Toeler <tyler.j.cooke+github@gmail.com>
2014-08-09 15:30:10 +1200

    Bump to 1.2.0

--- AuctionHouseRunes.lua
+++ AuctionHouseRunes.lua
@@ -17,7 +17,7 @@ local Logger
 -----------------------------------------------------------------------------------------------
 -- Constants
 -----------------------------------------------------------------------------------------------
-local ktVersion = {nMajor = 1, nMinor = 1, nPatch = 2}
+local ktVersion = {nMajor = 1, nMinor = 2, nPatch = 0}
 
 local ktDefaultSettings = {
 	tVersion = {
